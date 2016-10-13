6d57c60a0df2d6f919beeedbcf7ffb78bab632b5
RadarNyan <RadarNyan@moe9th.com>
2016-10-14 04:23:32 +0900

    修正：设置窗口
    * 翻译部分窗口
    * 翻译 DEBUG 菜单项和设置 ( 窗口未汉化 )
    * 调整用语：数据库 => 发送数据
    * 微调各处控件尺寸、位置

--- ElectronicObserver/Window/FormMain.cs
+++ ElectronicObserver/Window/FormMain.cs
@@ -637,7 +637,7 @@ namespace ElectronicObserver.Window {
 
 		private void StripMenu_File_SaveData_Load_Click( object sender, EventArgs e ) {
 
-			if ( MessageBox.Show( "セーブしていないレコードが失われる可能性があります。\r\nロードしますか？", "確認",
+			if ( MessageBox.Show( "可能会丢失尚未保存的记录。\r\n确认读取记录吗？", "要求确认",
 					MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2 )
 				== System.Windows.Forms.DialogResult.Yes ) {
 
@@ -652,7 +652,7 @@ namespace ElectronicObserver.Window {
 
 			using ( OpenFileDialog ofd = new OpenFileDialog() ) {
 
-				ofd.Title = "APIリストをロード";
+				ofd.Title = "载入 API 列表";
 				ofd.Filter = "API List|*.txt|File|*";
 				ofd.InitialDirectory = Utility.Configuration.Config.Connection.SaveDataPath;
 
@@ -664,7 +664,7 @@ namespace ElectronicObserver.Window {
 
 					} catch ( Exception ex ) {
 
-						MessageBox.Show( "API読み込みに失敗しました。\r\n" + ex.Message, "エラー",
+						MessageBox.Show( "API 读取失败。\r\n" + ex.Message, "错误",
 							MessageBoxButtons.OK, MessageBoxIcon.Error );
 
 					}
@@ -1059,7 +1059,7 @@ namespace ElectronicObserver.Window {
 			using ( var dialog = new OpenFileDialog() ) {
 
 				dialog.Filter = "Layout Archive|*.zip|File|*";
-				dialog.Title = "レイアウト ファイルを開く";
+				dialog.Title = "打开布局文件";
 
 
 				PathHelper.InitOpenFileDialog( Utility.Configuration.Config.Life.LayoutFilePath, dialog );
@@ -1080,7 +1080,7 @@ namespace ElectronicObserver.Window {
 			using ( var dialog = new SaveFileDialog() ) {
 
 				dialog.Filter = "Layout Archive|*.zip|File|*";
-				dialog.Title = "レイアウト ファイルの保存";
+				dialog.Title = "保存布局文件";
 
 
 				PathHelper.InitSaveFileDialog( Utility.Configuration.Config.Life.LayoutFilePath, dialog );
