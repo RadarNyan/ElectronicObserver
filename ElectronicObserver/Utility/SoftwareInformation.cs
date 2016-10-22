5f1f89549366a24056110e97027c79097602bb2c
RadarNyan <RadarNyan@moe9th.com>
2016-10-22 20:44:07 +0900

    Bump Version

--- ElectronicObserver/Utility/SoftwareInformation.cs
+++ ElectronicObserver/Utility/SoftwareInformation.cs
@@ -54,7 +54,7 @@ namespace ElectronicObserver.Utility {
 		/// </summary>
 		public static DateTime UpdateTime {
 			get {
-				return DateTimeHelper.CSVStringToTime( "2016/10/22 01:00:00" );
+				return DateTimeHelper.CSVStringToTime( "2016/10/22 20:42:58" );
 			}
 		}
 
